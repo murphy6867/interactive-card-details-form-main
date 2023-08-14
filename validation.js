a9584de647da94838f6b8ca89e25e242608f4b79
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-08-14 22:35:21 +0700

    Set  Redirection Page

--- validation.js
+++ validation.js
@@ -1,43 +1,9 @@
-  document.getElementById("card_form").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    
-    var cardholderName = document.getElementById("card-name").value;
-    var cardNumber = document.getElementById("card_number").value;
-    var expMonth = document.getElementById("card_exp_month").value;
-    var expYear = document.getElementById("card_exp_year").value;
-    var cvc = document.getElementById("card_cvc").value;
+const cardNum = document.getElementById('showCardnumber');
+    const inputForm = document.getElementById("card_information");
 
-    if (cardholderName.trim() === "") {
-      alert("Please enter a cardholder name.");
-      return;
-    }
+    inputForm.addEventListener("submit", function(event) {
+      event.preventDefault();
 
-    if (!isValidCardNumber(cardNumber)) {
-      alert("Please enter a valid card number.");
-      return;
-    }
+      window.location.href = "./pages/success.html"
 
-    if (!isValidExpirationDate(expMonth, expYear)) {
-      alert("Please enter a valid expiration date.");
-      return;
-    }
-
-    if (!isValidCVC(cvc)) {
-      alert("Please enter a valid CVC code.");
-      return;
-    }
-
-    window.location.href = "./pages/success.html";
-  });
-
-  function isValidCardNumber(cardNumber) {
-    return /^\d{16}$/.test(cardNumber);
-  }
-
-  function isValidExpirationDate(month, year) {
-    return /^\d{2}$/.test(month) && /^\d{2}$/.test(year);
-  }
-
-  function isValidCVC(cvc) {
-    return /^\d{3}$/.test(cvc);
-  }
+})
